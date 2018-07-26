da6399a7e5155dc9d4d68beead92e3832488ed85
Bunbury Admin <bunadmin@5070debian.indigo.schools.internal>
2018-07-26 10:40:47 +0800

    working on get_week

--- panels/get_week.php
+++ panels/get_week.php
@@ -6,13 +6,16 @@ include(config.php);
 */
 
 if (isset($_POST['date'])) {
+    $day = date('w', strtotime($_POST['date']));
+    $week = date('m-d-Y', strtotime('-'.$day.' days'));
 	
     $r_ids = explode(",",$_POST['r_ids']);
     foreach ($r_ids as $r_id) {
 
-        $stmt = $conn->prepare("SELECT * FROM resources WHERE r_id = :r_id")
+        $stmt = $conn->prepare("SELECT * FROM resources WHERE r_id = :r_id AND week = :week")
         $stmt->execute(array(
-            ":r_id" = $r_id
+            ":r_id" = $r_id,
+            ":week" = $week
         ));
 }
 
