146eed865c2b19a9f0f37ea28322dc9723b7bcdf
Bunbury Admin <bunadmin@5070debian.indigo.schools.internal>
2018-07-26 09:18:46 +0800

    git test

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+    <?PHP
+	session_start();
+        if (isset($_SESSION['username'])) {
+            echo "Hello ".$_SESSION['username']; 
+        } else {
+            echo "<a href='login.php'>Please Login</a>";
+        }
+    ?>
+    </body>
+</html>
