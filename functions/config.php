4edb2bdfb8227e309d44d839579c37334fe72bf9
root <root@5070debian.indigo.schools.internal>
2018-07-31 13:09:41 +0800

    functions etc

--- functions/config.php
+++ functions/config.php
@@ -3,4 +3,9 @@ $user = 'bookit';
 $pass = 'Holidays2';
 $conn = new PDO('mysql:host=localhost;dbname=bookit', $user, $pass);
 session_start();
+
+if(!isset($_SESSION['username'])) {
+    exit();
+}
+
 ?>
