1ed58b6cbb8d310bcb2b30ce264b013fa90a0be6
zeetum <timothy.blackburn@education.wa.edu.au>
2018-08-06 12:20:01 +0800

    Update create_catagory.php

--- functions/create_catagory.php
+++ functions/create_catagory.php
@@ -1,5 +1,12 @@
 <?PHP
+
+include('config.php');
+
+if (!isset($_POST['r_id']) || !isset($_POST['columns']))
+    exit();
+
 $query_string = "CREATE TABLE ".$_POST['catagory_name']." ( ";
+$query_string = " r_id VARCHAR(255) NOT NULL "
 $query_string .= " date VARCHAR(255) NOT NULL ";
 
 $columns = explode(",",$_POST['columns']);
@@ -7,5 +14,6 @@ foreach ($columns as $column) {
     $query_string .= $column." VARCHAR(255) ";
 }
 
-$query_string .= ")";
+$query_string .= " PRIMARY KEY(date, r_id) )";
+
 ?>
