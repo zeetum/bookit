aeb2786397cedf11f8d04091407a2b50f56696c7
zeetum <timothy.blackburn@education.wa.edu.au>
2018-08-06 13:23:31 +0800

    Update show_day.php

--- panels/show_day.php
+++ panels/show_day.php
@@ -2,7 +2,7 @@
 include_once('../functions/config.php');
 
 // Verify and sanatise input
-if (!isset($_POST['catagory']) || !isset($_POST['r_id']))
+if (!isset($_POST['catagory']))
 	exit();
 str_replace(";","",$_POST['catagory']);
 str_replace(",","",$_POST['catagory']);
@@ -12,42 +12,43 @@ else
 	$day = date("Y-m-d");
 
 // Prepare and execute the query
-$stmt = $conn->prepare("SELECT * FROM ".$_POST['catagory']." WHERE date = :date AND r_id = :r_id");
+$stmt = $conn->prepare("SELECT * FROM ".$_POST['catagory']." WHERE date = :date");
 $stmt->execute(array(
     ":date" => $day,
-	":r_id" => $_POST['r_id']
 ));
-$resources = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$timeslots = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$columns == '';
 
 // Display the results
 echo "<style>";
 include("show_week.css");
 echo "</style>";
 ?>
-
     <div class="divTable">
         <div class="divTableBody">
-            <div class="divTableRow">
-                <div class='divTableCell'>Resource</div>
-                <div class='divTableCell'>P1</div>
-                <div class='divTableCell'>P2</div>
-                <div class='divTableCell'>P3</div>
-                <div class='divTableCell'>P4</div>
-                <div class='divTableCell'>P5</div>
-            </div>
+<?PHP       foreach ($timeslots as $timeslot) {
+echo            "<div class='divTableRow'>";
+				foreach ($timeslot as $key => $value) {
+					if ($key != 'r_id' | $key != 'date') {
+echo 	        	    "<div class='divTableCell'>".$key."</div>";
+						$columns .= ",".$key;
+					}
+				}
+echo            "</div>";
+            }
 
-<?PHP       foreach ($resources as $resource) {
+<?PHP       foreach ($timeslots as $timeslot) {
             // Echo the resource name
 echo	    "<div class='divTableRow'>";
-
                $stmt = $conn->prepare("SELECT name FROM resources WHERE r_id = :r_id");
                $stmt->execute(array(
                    ":r_id" => $resource['r_id']
                ));
                $name = $stmt->fetch(PDO::FETCH_ASSOC)['name'];
 echo           "<form action='../panels/show_week.php' method='POST'>";
-echo               "<input type='hidden' name='r_id' value='".$resource['r_id']."'>";
-echo               "<input type='hidden' name='date' value='".$week."'>";
+echo               "<input type='hidden' name='r_id' value='".$timeslot['r_id']."'>";
+echo               "<input type='hidden' name='r_id' value='".$timeslot['catagory']."'>";
+echo               "<input type='hidden' name='date' value='".$day."'>";
 echo               "<input type='submit' value='".$name."'>";
 echo           "</form>";
 
@@ -56,10 +57,11 @@ echo           "</form>";
 echo           "<div class='divTableCell'>";
                    if ($resource[$timeslot] == '') {
 echo               "<form action='../functions/book_timeslot.php' method='POST'>";
-echo                   "<input type='hidden' name='r_id' value='".$resource['r_id']."'>";
-echo                   "<input type='hidden' name='t_id' value='".$timeslot."'>";
+echo                   "<input type='hidden' name='r_id' value='".$timeslot['r_id']."'>";
+echo                   "<input type='hidden' name='r_id' value='".$timeslot['catagory']."'>";
+echo                   "<input type='hidden' name='columns' value='".$columns."'>";
 echo                   "<input type='hidden' name='username' value='".$_SESSION['username']."'>";
-echo                   "<input type='hidden' name='date' value='".$week."'>";
+echo                   "<input type='hidden' name='date' value='".$day."'>";
 echo                   "<input type='submit' value='Book It!'>";
 echo               "</form>";
                    } else {
