6dc7d8ac726fbf2f536586850fd5613e4ddce6dd
root <root@5070debian.indigo.schools.internal>
2018-08-07 14:04:23 +0800

    creating new tables

--- panels/new_catagory.php
+++ panels/new_catagory.php
@@ -1,28 +1,34 @@
 <?PHP
-// Create a new table for the new resource
-include('config.php');
+// Enter new details for form
+echo "<form action='new_catagory.php' method=POST>";
+    echo "<input type=text name='catagory'></input>";
+    echo "<input type=text name='columns'></input>";
+    echo "<input type=submit>Submit</input>";
+echo "</form>";
+
+include("../functions/config.php");
 
 if (!isset($_POST['columns'])) {
     exit();
 }
 
 // sanitising the input
-str_replace(";","",$_POST['catagory_name']);
+str_replace(";","",$_POST['catagory']);
 
-$query_string = "CREATE TABLE ".$_POST['catagory_name']." ( ";
-$query_string = " r_id VARCHAR(255) NOT NULL ";
-$query_string .= " date VARCHAR(255) NOT NULL ";
+$query_string = "CREATE TABLE ".$_POST['catagory']." ( ";
+$query_string .= " r_id INT NOT NULL, ";
+$query_string .= " date VARCHAR(100) NOT NULL, ";
 
 $columns = explode(",",$_POST['columns']);
 foreach ($columns as $column) {
     // sanitising the input
     str_replace(";","",$column);
 
-    $query_string .= $column." VARCHAR(255) ";
+    $query_string .= $column." VARCHAR(255), ";
 }
 
 $query_string .= " PRIMARY KEY(date, r_id) )";
 
-$stmt = $conn->prepare("$query_string);
+$stmt = $conn->prepare($query_string);
 $stmt->execute();
 ?>
