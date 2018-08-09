49aa9d75aa534d5390b3a6755004d310f52817c8
root <root@5070debian.indigo.schools.internal>
2018-08-09 10:20:32 +0800

    fgetting show catagories tow ork

--- panels/show_catagories.js
+++ panels/show_catagories.js
@@ -2,9 +2,9 @@ function accordion_toggle(class_name) {
     var panels = document.getElementsByClassName(class_name);
 
     for (var i = 0; i < panels.length; i++) {
-        if (panels[i].style.display != "block")
-            panels[i].style.display = "block";
-	else
+        if (panels[i].style.display === "block")
             panels[i].style.display = "none";
+	else
+            panels[i].style.display = "block";
     }
 }
--- panels/show_catagories.php
+++ panels/show_catagories.php
@@ -1,15 +1,18 @@
 <?PHP
 include_once('../functions/config.php');
 
+if (isset($_GET['date']))
+	$day = $_GET['date'];
+else
+	$day = date("Y-m-d");
 
 // Get an array of resources in catagories in the form:
 //     $resources['catagory'] = array('resource1','resource2','resource3','etc...')
-$resources = array();
-
 $stmt = $conn->prepare("SHOW TABLES");
 $stmt->execute();
 $catagories = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$resources = array();
 foreach ($catagories as $catagory) if ($catagory['Tables_in_bookit'] != 'resources') {
 
     // get resources attached to each catagory
@@ -34,13 +37,37 @@ foreach ($catagories as $catagory) if ($catagory['Tables_in_bookit'] != 'resourc
 </head>
 <body>
     <div class="panel-group" id="accordion">
-<?PHP   // TODO: add buttons to accordion
+<?PHP
         foreach ($resources as $catagory => $r_ids) {
 echo    "<div class='accordion_catagory'>";
 echo        "<button class='accordion_button' onclick='accordion_toggle(\"".$catagory."_panel"."\")'>".$catagory."</button>";
+
+            // Day view of each catagory
+echo        "<div class='".$catagory."_panel accordion_resource'>";
+echo        "<form class='accordion_form' action='show_day.php' method='GET'>";
+echo            "<input type='hidden' name='catagory' value='".$catagory."'>";
+echo            "<input type='hidden' name='date' value='".$day."'>";
+echo            "<input class='accordion_button' type='submit' value='Day View'>";
+echo        "</form>";
+echo        "</div>";
+
             foreach ($r_ids as $r_id) {
+
+               $stmt = $conn->prepare("SELECT name FROM resources WHERE r_id = :r_id");
+               $stmt->execute(array(
+                   ":r_id" => $r_id
+               ));
+               $name = $stmt->fetch(PDO::FETCH_ASSOC)['name'];
+
+               // Week view of each catagory
 echo           "<div class='".$catagory."_panel accordion_resource'>";
-echo               "<p>".$r_id."</p>";
+	       // Select buttons for each week for each resource
+echo           "<form class='accordion_form' action='show_week.php' method='GET'>";
+echo               "<input type='hidden' name='r_id' value='".$r_id."'>";
+echo               "<input type='hidden' name='catagory' value='".$catagory."'>";
+echo               "<input type='hidden' name='date' value='".$day."'>";
+echo               "<input class='accordion_form_button' type='submit' value='".$name."'>";
+echo           "</form>";
 echo           "</div>";
             }
 echo    "<div>";
