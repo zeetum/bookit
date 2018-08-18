386a3c8146003bab11fd538b637bac3bc27f5999
Tim <timothy.blackburn@education.wa.edu.au>
2018-08-18 10:34:45 +0800

    redundant login

--- functions/config.php
+++ functions/config.php
@@ -5,7 +5,14 @@ $conn = new PDO('mysql:host=localhost;dbname=bookit', $user, $pass);
 session_start();
 
 if(!isset($_SESSION['username'])) {
-    header("Location:/bookit/panels/login.php",true);
+?>
+        <form action="../functions/ldap_auth.php" method='POST'>
+            <p>Please login</p>
+            <input type="text" name=username>Username</input>
+            <input type="password" name=password>Password</input>
+            <input type="submit">Login</input>
+        </form>
+<?PHP
 } else {
     echo "<a href='/bookit/functions/logout.php'>Logout</a>";
 }
