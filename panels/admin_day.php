1a918b3aca3e84b659a1986ac682bbee673fa174
root <root@5070debian.indigo.schools.internal>
2018-08-21 10:40:03 +0800

    admin day working

--- panels/admin_day.php
+++ panels/admin_day.php
@@ -24,16 +24,6 @@ do {
     $tomorrow = date('Y-m-d', strtotime($tomorrow.' +1 days'));
     $day_of_week = date('w', strtotime($tomorrow));
 } while ($day_of_week == 0 || $day_of_week == 6);
-echo        "<form action='admin_day.php' method='get'>";
-echo            "<input type='hidden' name='category' value='".$_GET['category']."'>";
-echo            "<input type='hidden' name='date' value='".$yesterday."'>";
-echo            "<input type='submit' value='yesterday'>";
-echo        "</form>";
-echo        "<form action='admin_day.php' method='get'>";
-echo            "<input type='hidden' name='category' value='".$_GET['category']."'>";
-echo            "<input type='hidden' name='date' value='".$tomorrow."'>";
-echo            "<input type='submit' value='tomorrow'>";
-echo        "</form>";
 
 // Prepare and execute the query
 $stmt = $conn->prepare("SELECT * FROM ".$_GET['category']." WHERE date = :date");
@@ -57,9 +47,23 @@ if (count($timeslots) == 0) {
 ?>
     <div class="divTable">
         <div class="divTableBody">
+<?PHP
+echo        "<div class='buttonWrapper'>";
+echo        "<form class=time_button action='admin_day.php' method='get'>";
+echo            "<input type='hidden' name='category' value='".$_GET['category']."'>";
+echo            "<input type='hidden' name='date' value='".$yesterday."'>";
+echo            "<input type='submit' value='yesterday'>";
+echo        "</form>";
+echo        "<form class=time_button action='admin_day.php' method='get'>";
+echo            "<input type='hidden' name='category' value='".$_GET['category']."'>";
+echo            "<input type='hidden' name='date' value='".$tomorrow."'>";
+echo            "<input type='submit' value='tomorrow'>";
+echo        "</form>";
+echo        "</div>";
 
-<?PHP       $timeslot = $timeslots[0];
-echo        "<div class='divTableRow'>";
+            $timeslot = $timeslots[0];
+echo        "<div class=divTableColumnWrapper>";
+echo        "<div class='divTableColumn'>";
 echo 	    "<div class='divTableCell'>".date('l', strtotime($date))."</div>";
 	        foreach ($timeslot as $key => $value) if (!($key == 'r_id' || $key == 'date'))
 echo 	    	    "<div class='divTableCell'>".$key."</div>";
@@ -67,7 +71,7 @@ echo        "</div>";
 
             foreach ($timeslots as $timeslot) {
             // Echo the resource column names
-echo	    "<div class='divTableRow'>";
+echo	    "<div class='divTableColumn'>";
                $stmt = $conn->prepare("SELECT name FROM resources WHERE r_id = :r_id");
                $stmt->execute(array(
                    ":r_id" => $timeslot['r_id']
@@ -75,26 +79,37 @@ echo	    "<div class='divTableRow'>";
                $name = $stmt->fetch(PDO::FETCH_ASSOC)['name'];
 	       
 	       // Select buttons for each week for each resource
-echo           "<form class='category_form' action='../panels/admin_week.php' method='GET'>";
+echo           "<form class='category_form' action='../panels/show_week.php' method='GET'>";
 echo               "<input type='hidden' name='r_id' value='".$timeslot['r_id']."'>";
 echo               "<input type='hidden' name='category' value='".$_GET['category']."'>";
 echo               "<input type='hidden' name='date' value='".$date."'>";
-echo               "<input class='category_button' type='submit' value='".$name."'>";
+echo               "<input class='resource_button' type='submit' value='".$name."'>";
 echo           "</form>";
 
 	       // Submit or display timeslot for each time column
                foreach ($timeslot as $key => $value) if (!($key == 'r_id' || $key == 'date')) {
                // Echo each user
 echo           "<div class='divTableCell'>";
-                   if ($value != '') {
-echo                    "<form action='../functions/delete_timeslot.php' method='POST'>";
-echo                        "<input type='hidden' name='r_id' value='".$timeslot['r_id']."'>";
-echo                        "<input type='hidden' name='date' value='".$date."'>";
-echo                        "<input type='hidden' name='category' value='".$_GET['category']."'>";
-echo                        "<input type='hidden' name='column' value='".$key."'>";
-echo                        "<input type='submit' value='Delete'>";
-echo                    "</form>";
-                   }
+                   if ($value == '') {
+echo               "<form action='../functions/book_timeslot.php' method='POST'>";
+echo                   "<input type='hidden' name='r_id' value='".$timeslot['r_id']."'>";
+echo                   "<input type='hidden' name='category' value='".$_GET['category']."'>";
+echo                   "<input type='hidden' name='column' value='".$key."'>";
+echo                   "<input type='hidden' name='username' value='".$_SESSION['username']."'>";
+echo                   "<input type='hidden' name='date' value='".$date."'>";
+echo                   "<input type='hidden' name='panel' value='admin_day'>";
+echo                   "<input type='submit' value='Book It!'>";
+echo               "</form>";
+                   } else {
+echo               "<form action='../functions/delete_timeslot.php' class='timeslot_taken' method='POST'>";
+echo                   "<input type='hidden' name='r_id' value='".$timeslot['r_id']."'>";
+echo                   "<input type='hidden' name='date' value='".$date."'>";
+echo                   "<input type='hidden' name='category' value='".$_GET['category']."'>";
+echo                   "<input type='hidden' name='column' value='".$key."'>";
+echo                   "<input type='hidden' name='panel' value='admin_day'>";
+echo                   "<input type='submit' value='Delete ".$value."'>";
+echo               "</form>";
+		   }
 echo           "</div>";
 	       }
 echo       "</div>";
