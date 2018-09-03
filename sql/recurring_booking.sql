dfa7e4a22ac0ba7775ade08f20cbb5e18dd80c95
zeetum <timothy.blackburn@education.wa.edu.au>
2018-09-03 12:38:10 +0800

    naming sql files

--- sql/recurring_booking.sql
+++ sql/recurring_booking.sql
@@ -1,8 +1,8 @@
-/* Make a booking to occur every day + weeks*7 */
+/* Make a booking to occur every 'jump' days */
 CREATE TABLE recurring_booking (
-	username VARCHAR(255) NOT NULL,
 	resource_table VARCHAR(255) NOT NULL,
 	column_name VARCHAR(255) NOT NULL,
-	day VARCHAR(255) NOT NULL,
-	jump VARCHAR(255) NOT NULL
+	username VARCHAR(255) NOT NULL,
+	start_day VARCHAR(255) NOT NULL, /* stomps with current date('y-m-d') */
+	jump VARCHAR(255) NOT NULL /* jump = "1 week" || "2 week" || "1 month" etc... */
 );
