f5fd08c26f06792c8425e64d6eb5394e036355fd
root <root@5070debian.indigo.schools.internal>
2018-09-06 15:27:23 +0800

    work to be done

--- panels/delete_recurring.php
+++ panels/delete_recurring.php
@@ -1,7 +1,19 @@
 <?PHP
 include_once($_SERVER["DOCUMENT_ROOT"].'/bookit/panels/boiler_header.html');
 include_once($_SERVER["DOCUMENT_ROOT"].'/bookit/functions/config.php');
-//
+// Get an array of all dates between lower and upper bound
+function get_dates($lower_bound, $jump, $upper_bound) {
+    $current = $lower_bound;
+    $dates = array();
+
+    while ($current < $upper_bound) {
+        array_push($dates, $current);
+        $current = date('Y-m-d', strtotime($current." +".$jump));
+    }
+    return $dates;
+}
+
+?>
 // Navigation Bar
 <div class='nav_bar'>
     <a class='active' id='home_button'><br></a>
@@ -10,6 +22,7 @@ include_once($_SERVER["DOCUMENT_ROOT"].'/bookit/functions/config.php');
     <a href='delete_category.php'>Delete Category</a>
     <a class='active' href='delete_resource.php'>Delete Resource</a>
 </div>
+<?PHP
 include_once($_SERVER["DOCUMENT_ROOT"].'/bookit/panels/admin_categories.php');
 
 // Select details for all resources
@@ -17,4 +30,13 @@ $stmt = $conn->prepare("SELECT * FROM recurring_booking");
 $stmt->execute();
 $bookings = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+echo "<div id='main_panel'>";
+         foreach ($bookings as $booking) {
+echo     "<form action='delete_resource.php' method='POST'>";
+             foreach($booking as $key => $value) 
+echo             "<input type='text' name='".$key."' value='".$value."' readonly>";
+echo         "<input type='submit' value='Delete'>";
+echo     "</form><br>";
+	 }
+echo "</div>";
 
